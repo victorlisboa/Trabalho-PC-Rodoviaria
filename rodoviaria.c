5392bb33425458a4065a35e09fc59b6bff54b3bd
Victor Hugo <victorhugof.lisboa@gmail.com>
2023-11-28 07:42:29 -0300

    setting threads

--- rodoviaria.c
+++ rodoviaria.c
@@ -3,18 +3,32 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define QNT_ONIBUS_NORMAL 10
+#define QNT_ONIBUS_NORMAL 5
 #define QNT_ONIBUS_ARTICULADO 2
+#define QNT_PASSAGEIROS 10
+#define QNT_BOXES 2
 
-void * f_articulado(void * id) {
+int boxes[QNT_BOXES];
+
+void * f_onibus(void * id) {
+    int a = *((int *) id);
+    
     while(1) {
         
     }
 }
 
-void * f_onibus(void * id) {
+void * f_articulado(void * id) {
+    int a = *((int *) id);
     while(1) {
+        
+    }
+}
 
+void * f_passageiro(void * id) {
+    int a = *((int *) id);
+    while(1) {
+        
     }
 }
 
@@ -22,6 +36,9 @@ int main() {
 
     pthread_t onibus[QNT_ONIBUS_NORMAL];
     pthread_t articulados[QNT_ONIBUS_ARTICULADO];
+    pthread_t passageiros[QNT_PASSAGEIROS];
+
+    // Criando threads e inicializando variaveis
     for(int i=0; i<QNT_ONIBUS_NORMAL; i++) {
         int *id = (int *) malloc(sizeof(int));
         *id = i;
@@ -29,10 +46,19 @@ int main() {
     }
     for(int i=0; i<QNT_ONIBUS_ARTICULADO; i++) {
         int *id = (int *) malloc(sizeof(int));
-        *id = i;
+        *id = i + QNT_ONIBUS_NORMAL;
         pthread_create(&articulados[i], NULL, f_articulado, (void *) id);
     }
+    for(int i=0; i<QNT_PASSAGEIROS; i++) {
+        int *id = (int *) malloc(sizeof(int));
+        *id = i;
+        pthread_create(&passageiros[i], NULL, f_passageiro, (void *) id);
+    }
+    for(int i=0; i<QNT_BOXES; i++) {
+        boxes[i] = -1;
+    }
 
+    pthread_join(onibus[0], NULL);
 
     return 0;
 }
